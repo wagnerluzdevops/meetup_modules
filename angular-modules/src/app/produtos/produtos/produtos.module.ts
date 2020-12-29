1d2b746043a3e43d45677b68b498fb6ae6e2a5af
wagnerluzdevops <wagnerpfonseca@gmail.com>
2020-12-29 10:19:20 -0300

    inclui material module em produtos module

--- angular-modules/src/app/produtos/produtos/produtos.module.ts
+++ angular-modules/src/app/produtos/produtos/produtos.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { AppMaterialModule } from '../../app-material/app-material.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AppMaterialModule
   ]
 })
 export class ProdutosModule { }

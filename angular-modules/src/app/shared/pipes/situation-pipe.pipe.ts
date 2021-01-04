908d3d37c1f03a4df0a1ce51d2c2b1a5bcbabc5c
wagnerluzdevops <wagnerpfonseca@gmail.com>
2021-01-04 13:57:06 -0300

    adiciona memo-decorator para evita chamadas redundantes

--- angular-modules/src/app/shared/pipes/situation-pipe.pipe.ts
+++ angular-modules/src/app/shared/pipes/situation-pipe.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import memo from 'memo-decorator';
 
 @Pipe({
   name: 'situation'
 })
 export class SituationPipe implements PipeTransform {
 
+  @memo()  
+  // @memo() Evita utilização redudante do pipe
   transform(value: number): any {
-    console.log('SituationPipe Called');
+    console.log('SituationPipe chamado!');
     const old = 2020 - value;
     if (old === 0) {
       return 'Tá novo';

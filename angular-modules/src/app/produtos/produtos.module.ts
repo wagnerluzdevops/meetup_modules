e526057c55aab624eb6a53afe91ab53b5b7ca834
Wagner Luz <wagnerpfonseca@gmail.com>
2021-02-23 16:33:38 -0300

    acerta codigo

--- angular-modules/src/app/produtos/produtos.module.ts
+++ angular-modules/src/app/produtos/produtos.module.ts
@@ -1,34 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProdutosRoutingModule } from './produtos-routing.module';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { SharedModule } from '../shared/shared.module';
-
-import { AppMaterialModule } from '../app-material/app-material.module';
-import { ProdutosListComponent } from './produtos-list/produtos-list.component';
-import { ProdutoEditComponent } from './produto-edit/produto-edit.component';
-
-
 import { DialogConfirmComponent } from 'src/app/shared/dialog-confirm/dialog-confirm.component';
 
-// import { SituationPipe } from '../shared/pipes/situation-pipe.pipe';
+import { AppMaterialModule } from '../app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
+import { ProdutoEditComponent } from './produto-edit/produto-edit.component';
+import { ProdutosListComponent } from './produtos-list/produtos-list.component';
+import { ProdutosRoutingModule } from './produtos-routing.module';
+
 
 @NgModule({
   declarations: [ProdutosListComponent, ProdutoEditComponent,
      DialogConfirmComponent,
-    //  SituationPipe
     ],
   imports: [
     CommonModule,
-    AppMaterialModule,   // APESAR DE TER SIDO IMPORTADO NO app.module, preciso importar aqui também !!
+    AppMaterialModule,  
     SharedModule,
     ProdutosRoutingModule,
     ReactiveFormsModule,
   ],
-//   exports: [ 
-//     SituationPipe
-//  ],
+
   entryComponents: [
     DialogConfirmComponent
  ]
